f684d70e308d70b87b09ee4ec4bd294770f9f53c
Daniel Wojda <wojdadan@gmail.com>
2015-10-24 13:12:57 +0200

    Initial gamecreator project. Generated from start.spring.io

---
+++ gamecreator/src/main/java/org/bluehack/GamecreatorApplication.java
@@ -0,0 +1,12 @@
+package org.bluehack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamecreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamecreatorApplication.class, args);
+    }
+}
