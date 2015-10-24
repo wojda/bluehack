5cf51ff3469708d8ed18a6579df33fd73b3d0272
Adam Madela <adam.madela@gmail.com>
2015-10-24 12:52:27 +0200

    game project initial setup

---
+++ game_templates/droplet/android/src/org/bluehack/dropletgame/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package org.bluehack.dropletgame.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.bluehack.dropletgame.DropletGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DropletGame(), config);
+	}
+}
---
+++ game_templates/droplet/desktop/src/org/bluehack/dropletgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.bluehack.dropletgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.bluehack.dropletgame.DropletGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DropletGame(), config);
+	}
+}
