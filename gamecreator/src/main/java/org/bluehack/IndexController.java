06188ae6051f8238353145414278bf6e873e33f3
Mateusz Gałan <galan.mateusz@gmail.com>
2015-10-24 14:46:33 +0200

    "Signin page added"

--- gamecreator/src/main/java/org/bluehack/IndexController.java
+++ gamecreator/src/main/java/org/bluehack/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @RequestMapping("/signin")
+    public String signin() {
+        return "signin";
+    }
 }
